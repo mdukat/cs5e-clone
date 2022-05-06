8817733e673827cc58d4ff6fc6666767cfefbfc4
mdukat <mateusz.dukat1@gmail.com>
2022-05-06 13:00:39 +0200

    sqlite example

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, request
 import json
 import random
+import sqlite3
 
 # Mateusz Dukat 2022
 
 app = Flask(__name__)
 
+# TODO render sheet template without forcing sheet code
 @app.route('/sheet')
 def sheet():
     #return render_template('sheet.html', sheet_code='1234')
@@ -39,7 +41,19 @@ def save():
     sheet_data = request.form.get('data') # JSON sheet data
     sheet_action = request.form.get('action') # known actions: save
 
-    return json.dumps({'status' : 200, 'code' : sheet_code, 'token' : pseudotokengenerator()})
+    if(sheet_code == ""):
+        return json.dumps({"status":404, "code": sheet_code, "response": "Not Found", "reason": "The requested resource or action was not found."})
+
+    if(sheet_token == None):
+        sheet_token = pseudotokengenerator()
+
+    conn = sqlite3.connect("exampledb.db")
+    cursor = conn.cursor()
+    cursor.execute(f"INSERT INTO sheets VALUES ('{sheet_code}', '{sheet_password}', '{sheet_data}', '{sheet_token}')")
+    conn.commit()
+    conn.close()
+
+    return json.dumps({'status' : 200, 'code' : sheet_code, 'token' : sheet_token})
 
 # Load and save use the same data.
 
@@ -47,5 +61,13 @@ def save():
 def load():
     sheet_code = request.args.get('code')
 
-    return json.dumps({"status":200,"code": sheet_code ,"data":{"page-1":{"alignment":"ali","background":"bkground","class":"classlevel","experience":"9999","name":"cname","player":"pname","race":"race"},"page-2":{"name-2":"cname"}}})
+    conn = sqlite3.connect("exampledb.db")
+    cursor = conn.cursor()
+    rows = cursor.execute(f"SELECT data FROM sheets WHERE code = '{sheet_code}'").fetchall()
+    conn.close()
+
+    if(len(rows) == 0):
+        return json.dumps({"status":404, "code": sheet_code, "response": "Not Found", "reason": "The requested resource or action was not found."})
+    else:
+        return json.dumps({"status":200, "code": sheet_code, "data": json.loads(rows[0][0])})
 
---
+++ create_table.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `sheets` (
+	`code` TEXT NOT NULL,
+	`password` TEXT NOT NULL,
+	`data` BLOB NOT NULL,
+	`token` TEXT NOT NULL
+);
